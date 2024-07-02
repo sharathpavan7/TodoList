8480ade45e6c149cdda484ac3f3f361dd3230adb
Sharath <sharathpavan7@gmail.com>
2024-07-02 23:40:09 +0530

    Search debounce and error message.

--- feature/src/main/java/com/sps/todo/feature/compose/TodoListScreen.kt
+++ feature/src/main/java/com/sps/todo/feature/compose/TodoListScreen.kt
@@ -1,24 +1,15 @@
 package com.sps.todo.feature.compose
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -32,29 +23,33 @@ import com.sps.todo.data.model.Todo
 import com.sps.todo.feature.R
 import com.sps.todo.feature.viewmodel.TodoViewModel
 
-
 @Composable
 fun TodoListScreen(
     navController: NavHostController,
     viewModel: TodoViewModel = hiltViewModel()
 ) {
-    val todoList = viewModel.todoList.collectAsState(initial = emptyList())
-    val searchQuery = viewModel.searchQuery.collectAsState().value
+    val todoList by viewModel.todoList.collectAsState(initial = emptyList())
+    val searchQuery by viewModel.searchQuery.collectAsState()
+    val errorEvent by viewModel.errorEvent.collectAsState()
 
     TodoListContent(
-        navController,
-        todoList = { todoList.value },
+        navController = navController,
+        todoList = { todoList },
         searchQuery = { searchQuery },
-        onSearchChange = { viewModel.setSearchQuery(it) })
+        onSearchChange = { viewModel.setSearchQuery(it) },
+        errorEvent = { errorEvent },
+        clearErrorEvent = { viewModel.clearErrorEvent() }
+    )
 }
-
 
 @Composable
 fun TodoListContent(
     navController: NavHostController,
     todoList: () -> List<Todo>,
     searchQuery: () -> String,
-    onSearchChange: (String) -> Unit
+    onSearchChange: (String) -> Unit,
+    errorEvent: () -> String?,
+    clearErrorEvent: () -> Unit
 ) {
     Scaffold(
         topBar = {
@@ -79,7 +74,6 @@ fun TodoListContent(
                 }
             } else {
                 Column(modifier = Modifier.padding(16.dp)) {
-
                     TextField(
                         modifier = Modifier
                             .fillMaxWidth()
@@ -88,6 +82,22 @@ fun TodoListContent(
                         onValueChange = { onSearchChange(it) },
                         placeholder = { Text(stringResource(R.string.search_todos)) }
                     )
+
+                    errorEvent()?.let {
+                        AlertDialog(
+                            onDismissRequest = { clearErrorEvent() },
+                            title = { Text(stringResource(R.string.error)) },
+                            text = { Text(stringResource(R.string.failed_to_add_todo)) },
+                            confirmButton = {
+                                Button(
+                                    colors = ButtonDefaults.buttonColors(Color.Blue),
+                                    onClick = { clearErrorEvent() }) {
+                                    Text(stringResource(R.string.dismiss))
+                                }
+
+                            }
+                        )
+                    }
 
                     LazyColumn {
                         items(todoList()) {
@@ -106,7 +116,6 @@ fun TodoListContent(
     )
 }
 
-
 @Composable
 @Preview(showBackground = false)
 fun TodoListContentPreview() {
@@ -114,5 +123,8 @@ fun TodoListContentPreview() {
         navController = NavHostController(LocalContext.current),
         todoList = { emptyList() },
         searchQuery = { "" },
-        onSearchChange = {})
+        onSearchChange = {},
+        errorEvent = { "" },
+        clearErrorEvent = {}
+    )
 }
