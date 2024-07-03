419baeae16090d9fd6984429304618adae37ccde
Sharath <sharathpavan7@gmail.com>
2024-07-03 20:28:52 +0530

    Bug fixes.

--- data/src/main/java/com/sps/todo/data/repository/TodoRepository.kt
+++ data/src/main/java/com/sps/todo/data/repository/TodoRepository.kt
@@ -2,9 +2,11 @@ package com.sps.todo.data.repository
 
 import com.sps.todo.data.dao.TodoDao
 import com.sps.todo.data.model.Todo
+import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+@ViewModelScoped
 class TodoRepository @Inject constructor(private val todoDao: TodoDao) {
     suspend fun insert(todo: Todo) {
         todoDao.insert(todo)
